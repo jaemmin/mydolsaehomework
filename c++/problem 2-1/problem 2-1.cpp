3f3b06bdb630d8457909d42834e8b634152d93fe
jaemmin <jaemin0017@gmail.com>
2022-05-03 18:33:16 +0900

    5주차

---
+++ c++/problem 2-1/problem 2-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int count;
+	cout << " 몇층? : ";
+	cin >> count;
+	for (int i = 0; i < count; i++) {
+		for (int j = count - 1; j > i; j--) {
+			printf(" ");
+		}
+
+		for (int j = 0; j < 2 * i + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+
+
+}
---
+++ c++/problem 2-2/problem 2-2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d, gcd;
+	cout << "a의 값은?: ";
+	cin >> a;
+	cout << "b의 값은?: ";
+	cin >> b;
+	cout << "c의 값은?: ";
+	cin >> c;
+	int a_output = a;
+	int b_output = b;
+	int c_output = c;
+	if (a > b)
+	{
+		while (b != 0)
+		{
+			d = a % b;
+			a = b;
+			b = d;
+		}
+		gcd = a;
+	}
+	else
+	{
+		while (a != 0)
+		{
+			d = b % a;
+			b = a;
+			a = d;
+		}
+		gcd = b;
+	}
+	if (gcd == 1)
+	{
+		cout << "방정식 ";
+		cout << a_output;
+		cout << "x+";
+		cout << b_output;
+		cout << "y=";
+		cout << c_output;
+		cout << "의 정수해는 없습니다.";
+	}
+	else if (c % gcd == 0)
+	{
+		cout << "방정식 ";
+		cout << a_output;
+		cout << "x+";
+		cout << b_output;
+		cout << "y=";
+		cout << c_output;
+		cout << "의 정수해는 있습니다.";
+	}
+	else
+	{
+		cout << "방정식 ";
+		cout << a_output;
+		cout << "x+";
+		cout << b_output;
+		cout << "y=";
+		cout << c_output;
+		cout << "의 정수해는 없습니다.";
+	}
+
+}
