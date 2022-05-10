8c9471f9ea6841453e256939c1c7f1b3d576bd34
jaemmin <jaemin0017@gmail.com>
2022-05-10 13:55:34 +0900

    6주차

---
+++ c++/problem 3-1/problem 3-1.cpp
@@ -0,0 +1,33 @@
+﻿// problem 3-1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int array[5];
+	int output_array[5];
+	cout << "1번째 요소는?:";
+	cin >> array[0];
+	cout << "2번째 요소는?:";
+	cin >> array[1];
+	cout << "3번째 요소는?:";
+	cin >> array[2];
+	cout << "4번째 요소는?:";
+	cin >> array[3];
+	cout << "5번째 요소는?:";
+	cin >> array[4];
+	for (int i = 0; i < 5; i++)
+	{
+		int j = 4 - i;
+		output_array[i] = array[j];
+	
+	}
+	cout << output_array[0];
+	cout << output_array[1];
+	cout << output_array[2];
+	cout << output_array[3];
+	cout << output_array[4];
+}
+
+
---
+++ c++/problem 3-2/problem 3-2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+int two_unit(int n);
+
+int main()
+{
+    int input = 0;
+    cout << "자연수 N의 값은?";
+    cin >> input;
+    cout << input;
+    cout << "은 ";
+    cout << two_unit(input);
+    cout << "자리 이진수입니다.";
+}
+
+int two_unit(int n)
+{
+    int checker = 1;
+    int double_checker = 2;
+    while (1)
+    {
+        if (n > double_checker)
+        {
+            checker++;
+            double_checker *= 2;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return checker;
+}
